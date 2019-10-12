1d41a13f335f8484b08e6a6093f29cf9ed5f901b
刘伟明 <weiming.liu@rokid.com>
2019-10-12 19:43:57 +0800

    添加语音测试相关功能： 1. 读取sdcard中glassOrderTest目录下的所有txt文件，生成测试页面入口列表； 2. 在相应测试页面读取文件逐行内容，生成相关测试指令组； 3. 开发指令测试页面相关UI，指令激活时会逐行显示在页面中；

---
+++ app/src/main/java/com/rokid/glass/instructdemo/test/OrderItemAct.java
@@ -0,0 +1,168 @@
+package com.rokid.glass.instructdemo.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.TextView;
+
+import com.rokid.glass.instruct.Integrate.InstructionActivity;
+import com.rokid.glass.instruct.entity.IInstructReceiver;
+import com.rokid.glass.instruct.entity.InstructConfig;
+import com.rokid.glass.instruct.entity.InstructEntity;
+import com.rokid.glass.instructdemo.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * Func: 测试应用指令测试页面
+ *
+ * @author: liuweiming
+ * @version: 1.0
+ * Create Time: 2019-07-29
+ */
+public class OrderItemAct extends InstructionActivity {
+
+    private static final String TAG = "AudioAi " + OrderItemAct.class.getSimpleName();
+    public static final String PARAM_ITEM_FILE = "item_file";
+
+    private TextView mInfoView;
+    private List<String> mOrderList;
+
+    @Override
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_item);
+
+        mInfoView = findViewById(R.id.show_text);
+        mInfoView.setMovementMethod(ScrollingMovementMethod.getInstance());
+        if (mOrderList != null && mOrderList.size() > 0) {
+            mInfoView.setText("设置测试指令：" + Arrays.toString(mOrderList.toArray()) + "\n\n\n"
+                    + "开始测试：\n");
+        } else {
+            mInfoView.setText("无设置测试指令\n\n\n" + "开始测试：\n");
+        }
+    }
+
+    @Override
+    public InstructConfig configInstruct() {
+        InstructConfig config = new InstructConfig();
+        config.setActionKey(OrderItemAct.class.getName() + InstructConfig.ACTION_SUFFIX);
+        mOrderList = getOrderByFile();
+        if (mOrderList != null) {
+            for (String order: mOrderList) {
+                if (!TextUtils.isEmpty(order)) {
+                    config.addInstructEntity(
+                            new InstructEntity()
+                                    .setName(order)
+                                    .setCallback(new IInstructReceiver() {
+                                        @Override
+                                        public void onInstructReceive(Activity act, String key, InstructEntity instruct) {
+                                            showOrder(key);
+                                        }
+                                    })
+                    );
+                }
+            }
+        }
+
+        return config;
+    }
+
+    @Override
+    public void onInstrucUiReady() {
+        super.onInstrucUiReady();
+        setMenuShowing(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * 是否拦截处理当前语音指令，拦截后之前配置的指令闭包不会被调用
+     * （可以用来提前处理一些指令，然后返回false）
+     * @param command
+     * @return true：拦截事件 false：不进行拦截
+     */
+    @Override
+    public boolean doReceiveCommand(String command) {
+        Log.d(TAG, "doReceiveCommand command = " + command);
+        return false;
+    }
+
+    private List<String> getOrderByFile(){
+        String fileName = getIntent().getStringExtra(PARAM_ITEM_FILE);
+        if (!TextUtils.isEmpty(fileName)) {
+            File file = new File(fileName);
+            if (file.exists() && file.isFile()) {
+                final List<String> tempList = new ArrayList<>();
+                BufferedReader br = null;
+                String readLine = null;
+                try {
+                    br = new BufferedReader(new FileReader(file));
+                    while ((readLine = br.readLine()) != null) {
+                        tempList.add(readLine);
+                    }
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (br != null) {
+                            br.close();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return tempList;
+            }
+        }
+
+        return null;
+    }
+
+    private void showOrder(String order) {
+        if (order != null && mInfoView != null) {
+            String info = getTimeText() + ", 激活：" + order + "\n";
+            mInfoView.append(info);
+            scrollTextEndLine();
+        }
+    }
+
+    private String getTimeText() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return simpleDateFormat.format(date);
+    }
+
+    private void scrollTextEndLine() {
+        if (mInfoView != null) {
+            int scrollAmount = mInfoView.getLayout().getLineTop(mInfoView.getLineCount()) - mInfoView.getHeight();
+            if (scrollAmount > 0) {
+                mInfoView.scrollTo(0, scrollAmount);
+            } else {
+                mInfoView.scrollTo(0, 0);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rokid/glass/instructdemo/test/OrderListAct.java
@@ -0,0 +1,153 @@
+package com.rokid.glass.instructdemo.test;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.rokid.glass.instruct.Integrate.InstructionActivity;
+import com.rokid.glass.instruct.entity.InstructConfig;
+import com.rokid.glass.instructdemo.R;
+import com.rokid.glass.instructdemo.instruct.HomeTestAct;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * Func: 测试应用启动页面
+ *
+ * @author: liuweiming
+ * @version: 1.0
+ * Create Time: 2019-07-29
+ */
+public class OrderListAct extends InstructionActivity {
+
+    private static final String TAG = "AudioAi " + OrderListAct.class.getSimpleName();
+
+    private static final String ORDER_TEST_DIR = "glassOrderTest";
+    private static final String FILE_SUFFIX = ".txt";
+
+    private LinearLayout mLayoutView;
+
+    @Override
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+
+        mLayoutView = findViewById(R.id.order_list);
+        findViewById(R.id.jump_demo).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OrderListAct.this, HomeTestAct.class);
+                startActivity(i);
+            }
+        });
+
+        initLinearBtn();
+    }
+
+    @Override
+    public InstructConfig configInstruct() {
+        InstructConfig config = new InstructConfig();
+        config.setActionKey(OrderListAct.class.getName() + InstructConfig.ACTION_SUFFIX);
+        return config;
+    }
+
+    @Override
+    public void onInstrucUiReady() {
+        super.onInstrucUiReady();
+
+        setMenuShowing(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * 是否拦截处理当前语音指令，拦截后之前配置的指令闭包不会被调用
+     * （可以用来提前处理一些指令，然后返回false）
+     * @param command
+     * @return true：拦截事件 false：不进行拦截
+     */
+    @Override
+    public boolean doReceiveCommand(String command) {
+        Log.d(TAG, "doReceiveCommand command = " + command);
+
+        if ("测试".equals(command)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void initLinearBtn() {
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + ORDER_TEST_DIR + File.separator;
+        File dirFile = new File(filePath);
+        if (dirFile.exists() && dirFile.isDirectory()) {
+            String[] fileNames = dirFile.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    Log.d("ordertest", "检测：" + name);
+                    if (name != null) {
+                        if (name.toLowerCase().endsWith(FILE_SUFFIX)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            });
+
+            if (fileNames != null) {
+                for (String fileName: fileNames) {
+                    Log.d("ordertest", "文件结果：" + fileName);
+                    addBtnToLinear(fileName, filePath);
+                }
+            }
+        }
+    }
+
+    private void addBtnToLinear(String fileName, String filePath) {
+        if (mLayoutView != null) {
+            Button button = new Button(this);
+            button.setText(fileName);
+            button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
+            button.setPadding(10, 10, 10, 10);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                button.setTextColor(getColor(R.color.white));
+            }
+            button.setBackgroundResource(R.drawable.btn_bg);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0, 0, 0, 5);
+
+            button.setTag(filePath + File.separator + fileName);
+
+            mLayoutView.addView(button, params);
+
+            button.setOnClickListener(mItemClick);
+        }
+    }
+
+    private View.OnClickListener mItemClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v != null) {
+                String fileName = (String) v.getTag();
+                if (fileName != null ) {
+                    Intent i = new Intent(OrderListAct.this, OrderItemAct.class);
+                    i.putExtra(OrderItemAct.PARAM_ITEM_FILE, fileName);
+                    startActivity(i);
+                }
+            }
+        }
+    };
+}
